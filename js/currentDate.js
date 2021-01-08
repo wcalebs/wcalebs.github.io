2af869792b7923df7482fea1792976b73cd76f7f
wcalebs <wcalebs@byui.edu>
2021-01-07 22:09:45 -0600

    Portal Commit

---
+++ js/currentDate.js
@@ -0,0 +1,5 @@
+//const options = {weekday: 'long', day:'numeric', month: 'long', year:'numeric'}
+//document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+
+let lastUpdated = document.lastModified;
+document.getElementById('updated').textContent = lastUpdated;
